701d5162715425bed7d4b4468c9cb014d687fc79
aquiles <jjoseaquiless@gmail.com>
2020-12-12 18:39:55 -0300

    printing good

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 from sensor import sensor
 import curses
+import time
 
 screen = curses.initscr()
 curses.noecho()
 screen.nodelay(True)
 flag = -1
-while flag != 0:
+while flag != ord("0"):
 	screen.clear()
 	screen.addstr(0, 0, "digite 0 para sair do programa")
-	flag = screen.getch()
 	temp, hum = sensor()
 	screen.addstr(1, 0, f"comodo central. Humidade: {hum} Temperatura {temp}")
+	flag = screen.getch()
+	time.sleep(1)
 
 curses.endwin()
