badf2cba000a0be67d146e51f004ddee498bb441
aquiles <jjoseaquiless@gmail.com>
2020-12-12 18:48:08 -0300

    menu scratch

--- main.py
+++ main.py
@@ -10,9 +10,20 @@ flag = -1
 while flag != ord("0"):
 	screen.clear()
 	screen.addstr(0, 0, "digite 0 para sair do programa")
+	screen.addstr(1, 0, "digite 1 para adicionar um novo dispositivo")
+	screen.addstr(2, 0, "digite 2 para setar o estado de um dispositivo")
+	screen.addstr(3, 0, "digite 3 para parar o alarme")
+
+	if(flag == ord("1")):
+		pass
+	elif (flag == ord("2")):
+		pass
+	elif (flag == ord("3")):
+		pass	
+
 	temp, hum = sensor()
-	screen.addstr(1, 0, f"comodo central. Humidade: {hum} Temperatura {temp}")
+	screen.addstr(4, 0, f"comodo central. Humidade: {hum} Temperatura {temp}")
 	flag = screen.getch()
-	time.sleep(1)
+	time.sleep(0.3)
 
 curses.endwin()
