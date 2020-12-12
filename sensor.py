ea7e7097b7035e291bde60d9f15f4eaa87c800fe
aquiles <jjoseaquiless@gmail.com>
2020-12-12 17:47:27 -0300

    add curses

--- sensor.py
+++ sensor.py
@@ -1,26 +1,31 @@
 import smbus2
 import bme280
-from time import *
+import time
+import curses
 
-def sensor():
-	porta_i2c = 1
-	endereco = 0x76
+# def sensor():
+screen = curses.initscr()
+curses.noecho()
+screen.nodelay(True)
+porta_i2c = 1
+endereco = 0x76
 
-	bus = smbus2.SMBus(porta_i2c)
-	calibracao_paramentros = bme280.load_calibration_params(bus, endereco)
+bus = smbus2.SMBus(porta_i2c)
+calibracao_paramentros = bme280.load_calibration_params(bus, endereco)
 
-	# timer = 0
-
-	while True:
-		dado = bme280.sample(bus, endereco, calibracao_paramentros)
-		temp = round(dado.temperature,2)
-		hum = round(dado.humidity,2)
-		press = round(dado.pressure,2)
-
-		print(f"aquiles T:{temp}")
-		print(f"U:{hum} P:{press}")
-		sleep(1)
+# timer = 0
+flag = -1
+while flag < 0:
+	dado = bme280.sample(bus, endereco, calibracao_paramentros)
+	temp = round(dado.temperature,2)
+	hum = round(dado.humidity,  2)
+	screen.deleteln()
+	screen.addstr(0,0,f"comodo central: Temperatura:{temp}  Umidade:{hum}")
+	screen.refresh()
+	flag = screen.getch()
+	time.sleep(1)
 	# timer +=1
+curses.endwin()
 '''
 print("ID: " + str(dado.id))
 print("Data/Hora: " + str(dado.timestamp))
